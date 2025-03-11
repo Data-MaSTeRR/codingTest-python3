62cad65c1260c9ba8ed89c61eef7bdcc3fad9660
Data-MaSTeRR <hyunwoo8504@gmail.com>
2025-03-11 12:26:03 +0900

    Feat: greedy 빈 구멍에 테이프 붙이기 - 백준 1449번

--- greedy-plumber.py
+++ greedy-plumber.py
@@ -1,11 +1,24 @@
-import sys
 
 N, L = map(int, input().split())
 
+# True, False 리스트를 사용
 coord = [False] * 1001
-for _ in range(N):
-    coord[int(sys.stdin.readline())] = True
+for i in map(int, input().split()):
+    coord[i] = True
 
-def solution(N, L, coord):
-    sorted_l = sorted(l)
+# 최초 구멍이 있는 곳에 테이프를 붙이고, L만큼 건너뛰고 새로운 구멍을 찾는 알고리즘
+def solution(L, coord):
+    answer = 0
+    i = 0
+    while i < 1001:
+        if coord[i] == True:
+            answer += 1
+            i += L
+        else:
+            i += 1
+
+    return answer
+
+print(solution(L, coord))
+
 
